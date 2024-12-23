4968af554a643711691585e90481ef48a6840838
Brahma <155218595+BrahmaGit24@users.noreply.github.com>
2024-12-23 17:35:33 +0530

    test case added

--- src/test/java/com/demo/bookstore/BookstoreApplicationTests.java
+++ src/test/java/com/demo/bookstore/BookstoreApplicationTests.java
@@ -1,13 +1,22 @@
 package com.demo.bookstore;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class BookstoreApplicationTests {
 
+	public static Logger logger = LoggerFactory.getLogger(BookstoreApplicationTests.class);
+	
 	@Test
 	void contextLoads() {
+		logger.info("Test case executing...");
+		assertEquals(true, true);
 	}
 
 }
